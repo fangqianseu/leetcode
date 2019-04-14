1311f92dc29eae471456a72b5b975c93a17cc4b2
fangqianseu <fangqiandndx@gmail.com>
2019-04-14 23:07:08 +0800

    leetcode-102 二叉树层次遍历 medium

---
+++ src/leetcode/_102_BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,51 @@
+/*
+Date:04.14,2019 22:58
+Given a binary tree, return the level order traversal of its nodes' values.
+(ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+*/
+package leetcode;
+
+import struct.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class _102_BinaryTreeLevelOrderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        ArrayList<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.offer(root);
+
+        while (!deque.isEmpty()) {
+            int size = deque.size();
+            ArrayList<Integer> list = new ArrayList<>();
+            while (size-- > 0) {
+                TreeNode node = deque.poll();
+                list.add(node.val);
+                if (node.left != null) deque.offer(node.left);
+                if (node.right != null) deque.offer(node.right);
+            }
+            res.add(list);
+        }
+
+        return res;
+    }
+}
