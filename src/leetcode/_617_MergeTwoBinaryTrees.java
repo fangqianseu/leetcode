da4d899e5a4ab7d4a3f552bf9f71eaba8ee6a170
fqseu <fangqiandndx@gmail.com>
2019-08-17 21:28:29 +0800

    leetcode-617 合并二叉树 easy

---
+++ src/leetcode/_617_MergeTwoBinaryTrees.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import struct.TreeNode;
+
+/**
+ * by qianfang, at 2019-08-17, 17:53
+ * <p>
+ * Given two binary trees and imagine that when you put one of them to cover the other,
+ * some nodes of the two trees are overlapped while the others are not.
+ * <p>
+ * You need to merge them into a new binary tree. The merge rule is that if two nodes overlap,
+ * then sum node values up as the new value of the merged node.
+ * Otherwise, the NOT null node will be used as the node of new tree.
+ **/
+public class _617_MergeTwoBinaryTrees {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return null;
+        }
+
+        if (t1 == null)
+            return t2;
+        if (t2 == null)
+            return t1;
+
+        TreeNode treeNode = new TreeNode(t1.val + t2.val);
+        treeNode.left = mergeTrees(t1.left, t2.left);
+        treeNode.right = mergeTrees(t1.right, t2.right);
+        return treeNode;
+    }
+}
