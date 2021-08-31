52a2329f8996d989db4209a8c0472c5853aa1e3e
fqseu <fangqiandndx@gmail.com>
2021-09-01 00:34:44 +0800

    最大子串

--- src/leetcode/_53_MaximumSubarray.java
+++ src/leetcode/_53_MaximumSubarray.java
@@ -13,13 +13,23 @@ Explanation: [4,-1,2,1] has the largest sum = 6.
 package leetcode;
 
 public class _53_MaximumSubarray {
+    /*
+    Given an integer array nums, find the contiguous subarray (containing at least one number)
+    which has the largest sum and return its sum.
+    A subarray is a contiguous part of an array.
+     */
     public int maxSubArray(int[] nums) {
-        int res = nums[0];
-        int sum = res;
+        int max = nums[0];
+        int temp = max;
         for (int i = 1; i < nums.length; i++) {
-            sum = Math.max(sum + nums[i], nums[i]);
-            res = Math.max(res, sum);
+            temp = Math.max(nums[i], temp + nums[i]);
+            max = Math.max(temp, max);
         }
-        return res;
+        return max;
+    }
+
+    public static void main(String[] args) {
+        _53_MaximumSubarray maximumSubarray = new _53_MaximumSubarray();
+        System.out.println(maximumSubarray.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
     }
 }
