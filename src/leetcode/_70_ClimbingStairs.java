9e7682c0f27bd447ef8352797f44c6c6a6f85f47
fqseu <fangqiandndx@gmail.com>
2021-09-03 00:59:52 +0800

    爬楼梯

--- src/leetcode/_70_ClimbingStairs.java
+++ src/leetcode/_70_ClimbingStairs.java
@@ -9,6 +9,11 @@ Note: Given n will be a positive integer.
 */
 package leetcode;
 
+/*
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ */
 public class _70_ClimbingStairs {
     /**
      * 递归法
@@ -17,9 +22,9 @@ public class _70_ClimbingStairs {
      * @return
      */
     public int climbStairs(int n) {
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-
+        if (n <= 2) {
+            return n;
+        }
         return climbStairs(n - 1) + climbStairs(n - 2);
     }
 
@@ -30,15 +35,16 @@ public class _70_ClimbingStairs {
      * @return
      */
     public int climbStairs2(int n) {
-        if (n < 3) return n;
-
+        if (n < 3) {
+            return n;
+        }
         int[] dp = new int[n + 1];
+        dp[0] = 0;
         dp[1] = 1;
         dp[2] = 2;
-
-        for (int i = 3; i <= n; i++)
-            dp[i] = dp[i-2] + dp[i-1];
-
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
         return dp[n];
     }
 
