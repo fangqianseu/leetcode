edfaf3bb3c44e5fb4a176b7d897d9e5ec584e7ab
“fqseu” <qianfseu@163.com>
2022-02-18 00:58:01 +0800

    合并有序数组

--- src/leetcode/_21_MergeTwoSortedLists.java
+++ src/leetcode/_21_MergeTwoSortedLists.java
@@ -7,48 +7,52 @@ Merge two sorted linked lists and return it as a sorted list.
 The list should be made by splicing together the nodes of the first two lists.
  */
 public class _21_MergeTwoSortedLists {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode res = new ListNode(1);
-        ListNode head = res;
-        while (null != l1 && null != l2) {
-            if (l1.val <= l2.val) {
-                res.next = l1;
-                l1 = l1.next;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode temp = new ListNode(0);
+        ListNode head = temp;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
             } else {
-                res.next = l2;
-                l2 = l2.next;
+                temp.next = list2;
+                list2 = list2.next;
             }
-            res = res.next;
+            temp = temp.next;
         }
 
-        if (null == l1) {
-            res.next = l2;
+        if (list1 == null && list2 == null) {
+            return head.next;
         }
-        if (null == l2) {
-            res.next = l1;
+        if (list1 != null) {
+            temp.next = list2;
         }
-
+        if (list2 != null) {
+            temp.next = list1;
+        }
         return head.next;
     }
 
     /*
     递归解法
      */
-    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
-        if (l1 == null)
-            return l2;
-        if (l2 == null)
-            return l1;
-
-        ListNode res;
-        if (l1.val <= l2.val) {
-            l1.next = mergeTwoLists2(l1.next, l2);
-            res = l1;
-        } else {
-            l2.next = mergeTwoLists2(l1, l2.next);
-            res = l2;
+    public ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 == null) {
+            return null;
         }
-
-        return res;
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        if (list1.val <= list2.val) {
+            list1.next = mergeTwoLists2(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists2(list1, list2.next);
+            return list2;
+        }
     }
 }
