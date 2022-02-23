1fa86beb1536a539afa662c3e5f56919193dc829
“fqseu” <qianfseu@163.com>
2022-02-24 00:05:37 +0800

    链表 判断是否有环

--- src/leetcode/_141_LinkedListCycle.java
+++ src/leetcode/_141_LinkedListCycle.java
@@ -22,14 +22,15 @@ public class _141_LinkedListCycle {
     快慢指针法
      */
     public boolean hasCycle(ListNode head) {
+        if (null == head) {
+            return false;
+        }
         ListNode fast = head;
         ListNode slow = head;
-
-        while (null != fast && null != fast.next &&
-                null != slow && null != slow.next) {
-            fast = fast.next;
-            slow = slow.next.next;
-            if (fast == slow) {
+        while (null != slow.next && null != fast.next && null != fast.next.next) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
                 return true;
             }
         }
