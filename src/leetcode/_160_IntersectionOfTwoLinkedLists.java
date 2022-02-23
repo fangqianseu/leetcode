61a9e96f8344cbfc6d4d713a7abf6da60448f5a3
“fqseu” <qianfseu@163.com>
2022-02-24 00:55:18 +0800

    链表交叉节点

---
+++ src/leetcode/_160_IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,48 @@
+/*
+Date:05.02,2019 22:05
+Write a program to find the node at which the intersection of two singly linked lists begins.
+*/
+package leetcode;
+
+import struct.ListNode;
+
+public class _160_IntersectionOfTwoLinkedLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lA = getLength(headA);
+        int lB = getLength(headB);
+        int diff = Math.abs(lA - lB);
+
+        if (lA > lB) {
+            headA = goHead(headA, diff);
+        } else {
+            headB = goHead(headB, diff);
+        }
+
+        while (null != headA && null != headB) {
+            if (headA == headB) {
+                return headA;
+            }
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return null;
+    }
+
+    private ListNode goHead(ListNode node, int between) {
+        while (between > 0) {
+            node = node.next;
+            between--;
+        }
+        return node;
+    }
+
+    private int getLength(ListNode head) {
+        ListNode l = head;
+        int length = 0;
+        while (null != l) {
+            length++;
+            l = l.next;
+        }
+        return length;
+    }
+}
