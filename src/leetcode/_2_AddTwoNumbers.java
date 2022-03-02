76560c4f9b31f1d65d834b5df248f2cdf2b95ac4
“fqseu” <qianfseu@163.com>
2022-03-03 00:07:18 +0800

    链表相加

--- src/leetcode/_2_AddTwoNumbers.java
+++ src/leetcode/_2_AddTwoNumbers.java
@@ -12,6 +12,30 @@ import struct.ListNode;
  * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
  **/
 public class _2_AddTwoNumbers {
+
+    public ListNode addTwoNumbersBest(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode h = head;
+
+        int add = 0;
+        while (l1 != null || l2 != null || add > 0) {
+            int temp = add;
+            if (null != l1) {
+                temp += l1.val;
+                l1 = l1.next;
+            }
+            if (null != l2) {
+                temp += l2.val;
+                l2 = l2.next;
+            }
+            h.next = new ListNode(temp % 10);
+            h = h.next;
+            add = temp / 10;
+        }
+
+        return head.next;
+    }
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(0);
         ListNode h = head;
