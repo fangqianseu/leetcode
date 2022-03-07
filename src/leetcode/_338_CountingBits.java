dcc5b9d2eaed236d7b956153505fe10ccbcd0e6e
“fqseu” <qianfseu@163.com>
2022-03-08 00:11:34 +0800

    计算位数

--- src/leetcode/_338_CountingBits.java
+++ src/leetcode/_338_CountingBits.java
@@ -5,6 +5,8 @@ calculate the number of 1's in their binary representation and return them as an
 */
 package leetcode;
 
+import java.util.Arrays;
+
 public class _338_CountingBits {
     /**
      * f[1] = (f[0]==0) + (1%1==1) = 1
@@ -19,9 +21,18 @@ public class _338_CountingBits {
      */
     public int[] countBits(int num) {
         int[] dp = new int[num + 1];
-        for (int i = 1; i <= num; i++) {
-            dp[i] = dp[i >> 1] + (1 & i);
+        Arrays.fill(dp, 0);
+
+        for (int i = 1; i < num + 1; i++) {
+
+            //  f(1110) = f(111) + 个位数是否为1
+            dp[i] = dp[i >> 1] + (i & 1);
         }
         return dp;
+    }
+
+    public static void main(String[] args) {
+        _338_CountingBits countingBits = new _338_CountingBits();
+        System.out.println(countingBits.countBits(5));
     }
 }
