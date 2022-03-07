3f1b395a6c86ba46df66b77f96eee0bc5de12500
“fqseu” <qianfseu@163.com>
2022-03-08 01:00:05 +0800

    三数和

--- src/leetcode/_15_3Sum.java
+++ src/leetcode/_15_3Sum.java
@@ -4,42 +4,76 @@ Find all unique triplets in the array which gives the sum of zero.
  */
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class _15_3Sum {
-    /*
-    1. i，l r 需要去重
-    2。 不能提前跳出循环
-     */
+
+
+    public List<List<Integer>> threeSumBest(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums == null || nums.length <= 2)
+            return ans;
+
+        Arrays.sort(nums); // O(nlogn)
+
+        for (int i = 0; i < nums.length - 2; i++) { // O(n^2)
+            if (nums[i] > 0)
+                break; // 第一个数大于 0，后面的数都比它大，肯定不成立了
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue; // 去掉重复情况
+            int target = -nums[i];
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] == target) {
+                    ans.add(new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right])));
+
+                    // 现在要增加 left，减小 right，但是不能重复，比如: [-2, -1, -1, -1, 3, 3, 3], i = 0, left = 1, right = 6, [-2, -1, 3] 的答案加入后，需要排除重复的 -1 和 3
+                    left++;
+                    right--; // 首先无论如何先要进行加减操作
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
+                } else if (nums[left] + nums[right] < target) {
+                    left++;
+                } else {  // nums[left] + nums[right] > target
+                    right--;
+                }
+            }
+        }
+        return ans;
+    }
+
     public List<List<Integer>> threeSum(int[] nums) {
-        ArrayList<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            int sum = -nums[i];
-            int l = i + 1, r = nums.length - 1;
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int target = nums[i];
+            int l = i + 1;
+            int r = nums.length - 1;
             while (l < r) {
-                if (nums[l] + nums[r] == sum) {
-                    res.add(new ArrayList<>(Arrays.asList(nums[i], nums[l], nums[r])));
-
-                    while (l < r && nums[l + 1] == nums[l]) l++;
-                    while (r > l && nums[r - 1] == nums[r]) r--;
-
+                if (nums[l] + nums[r] + target == 0) {
+                    String s = "" + nums[i] + nums[l] + nums[l];
+                    if (!set.contains(s)) {
+                        set.add(s);
+                        res.add(new ArrayList<>(Arrays.asList(nums[i], nums[l], nums[r])));
+                    }
                     l++;
                     r--;
-                } else if (nums[l] + nums[r] < sum)
+                } else if (nums[l] + nums[r] < -target) {
                     l++;
-                else r--;
+                } else {
+                    r--;
+                }
             }
-
-            while (i + 1 < nums.length - 2 && nums[i + 1] == nums[i]) i++;
         }
         return res;
     }
 
     public static void main(String[] args) {
-        new _15_3Sum().threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+        new _15_3Sum().threeSum(new int[]{-2, 0, 1, 1, 2});
     }
 }
