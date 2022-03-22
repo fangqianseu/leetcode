808012ceaab90e50554f7f5e09cccfb958d3bffa
“fqseu” <qianfseu@163.com>
2022-03-23 00:34:54 +0800

    重复数字二分法

--- src/leetcode/_34_FindFirstandLastPositionofElementinSortedArray.java
+++ src/leetcode/_34_FindFirstandLastPositionofElementinSortedArray.java
@@ -1,55 +1,67 @@
 /*
 Date: 04/01,2019, 10:50
-
-Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
-Your algorithm's runtime complexity must be in the order of O(log n).
-If the target is not found in the array, return [-1, -1].
 */
 package leetcode;
 
+/**
+ * Given an array of integers nums sorted in ascending order, find the starting and ending position of
+ * a given target value.
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ * If the target is not found in the array, return [-1, -1].
+ */
 public class _34_FindFirstandLastPositionofElementinSortedArray {
     public int[] searchRange(int[] nums, int target) {
-        int[] res = new int[2];
-        res[0] = -1;
-        res[1] = -1;
-        if (nums == null || nums.length == 0) return res;
-
-        res[0] = leftfind(nums, 0, nums.length - 1, target);
-        res[1] = rightfind(nums, 0, nums.length - 1, target);
-
-        return res;
-    }
-
-    // 相等时 也让 mid 变化
-    private int leftfind(int[] nums, int l, int r, double target) {
-        int res = -1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] >= target) {
-                r = mid - 1;
-            } else
-                l = mid + 1;
-            if (nums[mid] == target)
-                res = mid;
+        int[] res = new int[]{-1, -1};
+        if (null == nums || nums.length == 0) {
+            return res;
         }
+        res[0] = findLeft(nums, target);
+        res[1] = findRight(nums, target);
         return res;
     }
 
-    private int rightfind(int[] nums, int l, int r, double target) {
-        int res = -1;
+    private int findRight(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        int temp = -1;
         while (l <= r) {
             int mid = l + (r - l) / 2;
             if (nums[mid] <= target) {
                 l = mid + 1;
-            } else
+
+            } else {
                 r = mid - 1;
-            if (nums[mid] == target)
-                res = mid;
+            }
+            if (nums[mid] == target) {
+                temp = mid;
+            }
         }
-        return res;
+        return temp;
     }
 
+    private int findLeft(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        int temp = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] >= target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+
+            if (nums[mid] == target) {
+                temp = mid;
+            }
+        }
+        return temp;
+    }
+
+
     public static void main(String[] args) {
-        new _34_FindFirstandLastPositionofElementinSortedArray().searchRange(new int[]{5,7,7,8,8,10}, 8);
+        _34_FindFirstandLastPositionofElementinSortedArray elementinSortedArray = new _34_FindFirstandLastPositionofElementinSortedArray();
+        int[] ints = elementinSortedArray.searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8);
+        for (int anInt : ints) {
+            System.out.println(anInt);
+        }
     }
 }
