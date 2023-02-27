0a25af32a0df0903583195309e4c78187be34284
“fqseu” <qianfseu@163.com>
2023-02-27 22:40:17 +0800

    最长回文子串

--- src/leetcode/_5_LongestPalindromicSubstring.java
+++ src/leetcode/_5_LongestPalindromicSubstring.java
@@ -4,22 +4,27 @@ Given a string s, return the longest palindromic substring in s
  */
 package leetcode;
 
+import java.util.Objects;
+
 public class _5_LongestPalindromicSubstring {
 
     // 分别以每个字符作为中心 向两边检测是否为回文
-    public String longestPalindrome2(String s) {
+    public String longestPalindrome(String s) {
+        if (Objects.equals(s, "")) {
+            return "";
+        }
         String res = "";
         for (int i = 0; i < s.length(); i++) {
-            // 偶数回文
+            // 偶数回文场景
             res = checkPalindromePalindrome(s, i, i + 1, res);
-            // 奇数回文
+            //奇数回文场景
             res = checkPalindromePalindrome(s, i, i, res);
         }
         return res;
-
     }
 
     private String checkPalindromePalindrome(String s, int from, int to, String res) {
+        // 符合回文特征 from、to向两边扩散
         while (from >= 0 && to < s.length() && s.charAt(from) == s.charAt(to)) {
             from--;
             to++;
@@ -29,32 +34,10 @@ public class _5_LongestPalindromicSubstring {
     }
 
     //    ===================================================
-    // 暴力法 超时 O(n^2)
-    public String longestPalindrome(String s) {
-        String res = "";
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i + 1; j <= s.length(); j++) {
-                String temp = s.substring(i, j);
-                if (isPalindrome(temp)) {
-                    res = res.length() > temp.length() ? res : temp;
-                }
-            }
-        }
-        return res;
-    }
-
-    public boolean isPalindrome(String s) {
-        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return false;
-            }
-        }
-        return true;
-    }
 
 
     public static void main(String[] args) {
         _5_LongestPalindromicSubstring s = new _5_LongestPalindromicSubstring();
-        System.out.println(s.longestPalindrome2("cbbd"));
+        System.out.println(s.longestPalindrome("cbabd"));
     }
 }
