b630ec4c474f7f86a7c7eccc4a082d06287259cb
“fqseu” <qianfseu@163.com>
2023-03-01 00:29:01 +0800

    合并有序数组

--- src/leetcode/_21_MergeTwoSortedLists.java
+++ src/leetcode/_21_MergeTwoSortedLists.java
@@ -11,26 +11,25 @@ public class _21_MergeTwoSortedLists {
         ListNode temp = new ListNode(0);
         ListNode head = temp;
 
-        while (list1 != null && list2 != null) {
-            if (list1.val <= list2.val) {
-                temp.next = list1;
-                list1 = list1.next;
-            } else {
+        while (list1 != null || list2 != null) {
+            if (list1 != null && list2 != null) {
+                if (list1.val <= list2.val) {
+                    temp.next = list1;
+                    list1 = list1.next;
+                } else {
+                    temp.next = list2;
+                    list2 = list2.next;
+                }
+            } else if (list1 == null) {
                 temp.next = list2;
                 list2 = list2.next;
+            } else {
+                temp.next = list1;
+                list1 = list1.next;
             }
             temp = temp.next;
         }
 
-        if (list1 == null && list2 == null) {
-            return head.next;
-        }
-        if (list1 != null) {
-            temp.next = list1;
-        }
-        if (list2 != null) {
-            temp.next = list2;
-        }
         return head.next;
     }
 
