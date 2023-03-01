2b7d9aa1ca1ff77aa8f1d477e83bd00f770c0731
“fqseu” <qianfseu@163.com>
2023-03-01 23:15:57 +0800

    括号生成

--- src/leetcode/_22_GenerateParentheses.java
+++ src/leetcode/_22_GenerateParentheses.java
@@ -19,25 +19,27 @@ import java.util.List;
 public class _22_GenerateParentheses {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
-        helper(res, 0, 0, n, "");
+        generateHelp("", n, n, res);
         return res;
     }
 
-    // l :（ 已经生成个数
-    // r： ) 已经生成个数
-    private void helper(List<String> res, int l, int r, int n, String s) {
-        if (l == n && r == n) {
+    // left 剩余左括号 right 剩余右括号
+    private void generateHelp(String s, int left, int right, List<String> res) {
+        // 都生成完了 加入结果集
+        if (left == 0 && right == 0) {
             res.add(s);
-            return;
         }
-        if (l < n) {
-            helper(res, l + 1, r, n, s + "(");
+        // 左括号 可以直接生成
+        if (left > 0) {
+            generateHelp(s + "(", left - 1, right, res);
         }
-        // 注意 一定是要 r 小于 l 而不是 n； 不然 () 不会闭合
-        if (r < l) {
-            helper(res, l, r + 1, n, s + ")");
+        //右括号 必须和左括号对应生成
+        if (right > 0 && left < right) {
+            generateHelp(s + ")", left, right - 1, res);
+
         }
     }
+
 
     public static void main(String[] args) {
         _22_GenerateParentheses generateParentheses = new _22_GenerateParentheses();
