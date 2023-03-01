b41c1a4c2080ff9fe577f3fa566ae7ce83e8e507
“fqseu” <qianfseu@163.com>
2023-03-02 00:31:06 +0800

    下一个排列

--- src/leetcode/_31_NextPermutation.java
+++ src/leetcode/_31_NextPermutation.java
@@ -26,47 +26,51 @@ public class _31_NextPermutation {
      * @param nums
      */
     public void nextPermutation(int[] nums) {
-        if (null == nums || nums.length < 1) {
+        if (nums == null || nums.length <= 1) {
             return;
         }
-        int r = nums.length - 1 - 1;
-        // 寻找走又想着 第一个 非递增数列 的下标 即 r下标的为 最小数
-        for (; r >= 0; r--) {
-            if (nums[r] < nums[r + 1]) {
+        // 从右到左 找到第一个非递增的下标
+        int index = nums.length - 1;
+        while (index > 0 && nums[index - 1] >= nums[index]) {
+            index--;
+        }
+        // 如果index为0 则说明数组为递减序列 直接翻转就好
+        if (index < 1) {
+            reserve(nums, 0, nums.length - 1);
+            return;
+        }
+
+        // 此时  [index-1,index]为递增  【index,end]为递减
+
+        // 在【index,end]中 从有道做 选择第一个大于 num[index-1]的值 和 index-1交换
+        int t = nums[index - 1];
+        for (int i = nums.length - 1; i >= index; i--) {
+            if (nums[i] > t) {
+                swap(nums, i, index - 1);
                 break;
             }
         }
 
-        // r下标小于0 证明整个数列为递减数列，返回递增数列 即数组从0开始翻转
-        if (r < 0) {
-            reverse(nums, 0);
-            return;
-        }
-
-        // 从右向左 找出第一个大于下标 r 的数字 交换它和r下标的顺序
-        for (int i = nums.length - 1; i > r; i--) {
-            if (nums[i] > nums[r]) {
-                int temp = nums[i];
-                nums[i] = nums[r];
-                nums[r] = temp;
-                break;
-            }
-        }
-        reverse(nums, r + 1);
+        //剩下的 [index,end] 仍然为递减序列  翻转就好
+        reserve(nums, index, nums.length - 1);
     }
 
-    // 翻转 数组 r下标 之后的数字顺序
-    private void reverse(int[] nums, int r) {
-        for (int i = r, j = nums.length - 1; i < j; i++, j--) {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
+    private void reserve(int[] nums, int l, int r) {
+        for (; l < r; l++, r--) {
+            swap(nums, l, r);
         }
     }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
 
     public static void main(String[] args) {
 //        new _31_NextPermutation().nextPermutation(new int[]{1, 2, 3, 4, 6, 5});
 //        new _31_NextPermutation().nextPermutation(new int[]{3, 2, 1});
-        new _31_NextPermutation().nextPermutation(new int[]{1, 3, 2});
+        new _31_NextPermutation().nextPermutation(new int[]{5, 1, 1});
     }
 }
