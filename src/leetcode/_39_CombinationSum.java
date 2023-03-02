519127cb6312de004111695750c7832a04cde125
“fqseu” <qianfseu@163.com>
2023-03-03 00:13:22 +0800

    组合总和

--- src/leetcode/_39_CombinationSum.java
+++ src/leetcode/_39_CombinationSum.java
@@ -20,31 +20,27 @@ public class _39_CombinationSum {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(candidates);
-        List<Integer> current = new ArrayList<>();
-        helper(candidates, 0, target, current, res);
+        helper(candidates, 0, target, new ArrayList<>(), res);
         return res;
     }
 
-    private void helper(int[] candidates, int index, int target, List<Integer> current, List<List<Integer>> res) {
-        if (index >= candidates.length) {
-            return;
-        }
+    private void helper(int[] candidates, int i, int target, List<Integer> current, List<List<Integer>> res) {
         if (target == 0) {
-            // 不能直接传 current，这个对象是可变的
             res.add(new ArrayList<>(current));
             return;
         }
-        if (target < 0) {
+        if (i >= candidates.length) {
             return;
         }
-
-        for (int i = index; i < candidates.length; i++) {
-            while (i + 1 < candidates.length && candidates[i] == candidates[i + 1]) {
-                i++;
+        if (target < candidates[i]) {
+            return;
+        }
+        for (int index = i; index < candidates.length && target >= candidates[index]; index++) {
+            if (target >= candidates[index]) {
+                current.add(candidates[index]);
+                helper(candidates, index, target - candidates[index], current, res);
+                current.remove(current.size() - 1);
             }
-            current.add(candidates[i]);
-            helper(candidates, i, target - candidates[i], current, res);
-            current.remove(current.size() - 1);
         }
     }
 
