c70d1ae4294e9e9f146d0213f5bc41c5c92dd740
“fqseu” <qianfseu@163.com>
2023-03-06 23:57:14 +0800

    爬楼梯

--- src/leetcode/_70_ClimbingStairs.java
+++ src/leetcode/_70_ClimbingStairs.java
@@ -30,14 +30,13 @@ public class _70_ClimbingStairs {
         if (n < 3) {
             return n;
         }
-        int[] dp = new int[n + 1];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return dp[n];
+        return dp[n - 1];
     }
 
     public static void main(String[] args) {
