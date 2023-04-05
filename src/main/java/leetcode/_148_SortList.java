e9ec2bded740a01de1d451c08c9bb8f3221b3887
“fqseu” <qianfseu@163.com>
2023-04-06 00:43:50 +0800

    链表排序

--- src/main/java/leetcode/_148_SortList.java
+++ src/main/java/leetcode/_148_SortList.java
@@ -14,54 +14,56 @@ import struct.ListNode;
 
 public class _148_SortList {
     public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null)
+
+        // 注意判断 head.next 不为空  否则mid 和head一致 会无限递归
+        if (null == head || head.next == null) {
             return head;
-
-        ListNode mid = head, fast = head;
-        ListNode pre = null;
-
-        // 找到listnode 的中点
-        while (fast != null && fast.next != null) {
-            pre = mid;
-            mid = mid.next;
-            fast = fast.next.next;
         }
 
-        // 从 中点前截断
-        pre.next = null;
+        // 快慢指针 找出中点
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode mid = slow.next;
 
-        ListNode l1 = sortList(head);
-        ListNode l2 = sortList(mid);
+        //从中点截断
+        slow.next = null;
 
-        return merge(l1, l2);
+        // 递归  将链表分割为单节点
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+
+        // 归并排序
+        return merge(left, right);
     }
 
-    private ListNode merge(ListNode left, ListNode right) {
-        ListNode head = new ListNode(0);
+    private ListNode merge(ListNode l, ListNode r) {
+        ListNode head = new ListNode();
         ListNode temp = head;
-        while (left != null || right != null) {
-            if (left != null && right != null) {
-                if (left.val <= right.val) {
-                    temp.next = left;
-                    temp = temp.next;
-                    left = left.next;
+        while (null != l || null != r) {
+            if (null != l && null != r) {
+                if (l.val <= r.val) {
+                    temp.next = l;
+                    l = l.next;
                 } else {
-                    temp.next = right;
-                    temp = temp.next;
-                    right = right.next;
+                    temp.next = r;
+                    r = r.next;
                 }
-            } else if (left == null) {
-                temp.next = right;
-                temp = temp.next;
-                right = right.next;
+            } else if (l != null) {
+                temp.next = l;
+                l = l.next;
             } else {
-                temp.next = left;
-                temp = temp.next;
-                left = left.next;
+                temp.next = r;
+                r = r.next;
             }
+            temp = temp.next;
         }
         return head.next;
     }
+
 
     public static void main(String[] args) {
         ListNode l1 = new ListNode(4);
