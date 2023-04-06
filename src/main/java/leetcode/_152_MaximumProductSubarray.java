a1d84b954d95e474108fa6317f23ba1448839205
“fqseu” <qianfseu@163.com>
2023-04-07 00:28:32 +0800

    最大子数组乘积

--- src/main/java/leetcode/_152_MaximumProductSubarray.java
+++ src/main/java/leetcode/_152_MaximumProductSubarray.java
@@ -14,44 +14,47 @@ package leetcode;
 
 public class _152_MaximumProductSubarray {
     /**
-     * dp[i][j] 表示从i到j的乘积
-     * dp[i][j] = dp[i][j-1] * num[j]
-     * 可用 pre代替dp[i][j-1]， 优化空间
+     * nums[i] 有正有负，需要同时保存 当前位置 最大 最小的值
+     *
      * @param nums
      * @return
      */
     public int maxProduct(int[] nums) {
-        int res = Integer.MIN_VALUE;
-        int[][] dp = new int[nums.length][nums.length];
-        for (int i = 0; i < nums.length; i++)
-            for (int j = i; j < nums.length; j++) {
-                if (i == j)
-                    dp[i][j] = nums[i];
-                else
-                    dp[i][j] = nums[j] * dp[i][j - 1];
-                res = Math.max(res, dp[i][j]);
-            }
+        if (nums.length < 1) {
+            return 0;
+        }
+        int[] max = new int[nums.length];
+        int[] min = new int[nums.length];
+        max[0] = nums[0];
+        min[0] = nums[0];
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            max[i] = Math.max(max[i - 1] * nums[i], Math.max(min[i - 1] * nums[i], nums[i]));
+            min[i] = Math.min(max[i - 1] * nums[i], Math.min(min[i - 1] * nums[i], nums[i]));
+            res = Math.max(res, max[i]);
+        }
         return res;
     }
 
     /**
      * max 保存最大值情况
      * min 保存最小值，有负数 则变成最大
+     *
      * @param nums
      * @return
      */
     public int maxProduct2(int[] nums) {
         int res = nums[0];
-        int max = nums[0],min = nums[0];
+        int max = nums[0], min = nums[0];
 
-        for (int i = 1; i < nums.length ;i++){
-            if (nums[i] < 0){
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < 0) {
                 int t = max;
                 max = min;
                 min = t;
             }
-            max = Math.max(max*nums[i],nums[i]);
-            min = Math.min(min*nums[i],nums[i]);
+            max = Math.max(max * nums[i], nums[i]);
+            min = Math.min(min * nums[i], nums[i]);
 
             if (max > res)
                 res = max;
@@ -59,6 +62,7 @@ public class _152_MaximumProductSubarray {
 
         return res;
     }
+
     public static void main(String[] args) {
         new _152_MaximumProductSubarray().maxProduct(new int[]{2, 3, -2, 4});
     }
